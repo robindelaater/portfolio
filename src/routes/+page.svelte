89ba319799dc4508b464703df2b2296d4e41e489
Robin de Laater <robin.delaater@shopmonkey.nl>
2023-03-17 19:26:27 +0100

    First attempts at something remotely original

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="sticky top-0 bg-gradient-to-tr from-indigo-500 to-purple-400">
+	<header class="flex items-center justify-between w-full container mx-auto py-8">
+		<p class="font-semibold text-white">Robin de Laater</p>
+		<nav class="flex items-center gap-6 text-white">
+			<a href="/somewhere">Somewhere</a>
+			<a href="/nowhere">Nowhere</a>
+		</nav>
+	</header>
+
+	<div class="flex items-center justify-center w-full container mx-auto">
+		<p class="font-bold text-6xl leading-normal text-white">
+			Hi! My name is Robin de Laater.<br /> I'm a creative developer and love starting new projects.
+		</p>
+		<img src="/memoji.png" alt="🦍" />
+	</div>
+</section>
+
+<section class="sticky h-[100vh] bg-white z-10">
+	<div class="container mx-auto py-8">
+		<h2 class="text-xl">About me</h2>
+	</div>
+</section>
